09ab162a1ee964af232a810dbb4b85e0d3575c98
GodwinZuberu <onimico4eva@gmail.com>
2022-09-08 14:33:52 -0700

    Task done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+
+ * Simple program that uses print function
+
+ *
+
+ * Return: a string
+
+ */
+
+int main(void)
+
+{
+
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return (0);
+
