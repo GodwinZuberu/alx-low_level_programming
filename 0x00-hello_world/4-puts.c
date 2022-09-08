d0550cad8d083b955a75c7757b5f85d9ef4b59fc
GodwinZuberu <onimico4eva@gmail.com>
2022-09-08 14:49:20 -0700

    used betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 
 /**
-
  * Simple program that uses print function
-
  *
-
  * Return: a string
-
  */
-
 int main(void)
-
 {
-
     puts("\"Programming is like building a multilingual puzzle");
-
     return (0);
 }
