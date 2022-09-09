2004648086d5e06c2f4b0a3d6e12670702f35f7a
GodwinZuberu <onimico4eva@gmail.com>
2022-09-08 21:34:54 -0700

    minor edit n task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
 * main -entry point of the code
 * this program Write a C program that prints exactly with proper grammar, but the outcome is a piece of art.
-* Return 0
+* Return: 0
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
