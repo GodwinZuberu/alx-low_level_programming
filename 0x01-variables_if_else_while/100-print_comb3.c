fc73dad939184331eb7222305301f5c95a58d7ca
GodwinZuberu <onimico4eva@gmail.com>
2022-09-11 14:09:52 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,17 +6,17 @@
 int main()
 {
 	int n1 = 0, n2;
-	while(n2 <= 9)
+	while (n2 <= 9)
 	{
 		n2 = 0;
-		while(n2 <= 9)
+		while (n2 <= 9)
 		{
-			if(n1 !=n2 && n1 < n2)
+			if (n1 !=n2 && n1 < n2)
 			{
 				putchar(n1+48);
 				putchar(n2+48);
 
-				if(n1+n2 != 17)
+				if (n1+n2 != 17)
 				{
 					putchar(',');
 					putchar(' ');
