feaced76cb95f501083698b5a92c8e1e0d8d6f61
GodwinZuberu <onimico4eva@gmail.com>
2022-09-11 15:00:57 -0700

    done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 /*
-* main - print if the number is postive, zero, or negative
+* main - Inventing is a combination of brains and materials. The more brains you use, the less material you need
 *
-* Description: Print
+* Description: using the main function
+* Inventing is a combination of brains and materials. The more brains you use, the less material you need
 *
+* * Return: 0
 *
 */
 
 int main(void)
 {
 	int n1 = 0, n2;
+
 	while (n2 <= 9)
 	{
 		n2 = 0;
+
 		while (n2 <= 9)
 		{
 			if (n1 != n2 && n1 < n2)
