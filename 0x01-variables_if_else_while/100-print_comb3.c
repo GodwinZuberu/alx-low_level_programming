637b79accea9eb02b73c656d8b473de8c9689c4b
GodwinZuberu <onimico4eva@gmail.com>
2022-09-11 15:57:24 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
-/*
+/**
 * main - Inventing is a combination of brains and materials. The more brains you use, the less material you need
 *
 * Description: using the main function
 * Inventing is a combination of brains and materials. The more brains you use, the less material you need
 *
-* * Return: 0
+* Return: 0
 *
 */
 
 int main(void)
 {
-	int n1 = 0, n2;
-	while (n2 <= 9)
+	int d;
+	for (d = 0; d < 100; d++)
 	{
-		n2 = 0;
-		while (n2 <= 9)
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 90)
 		{
-			if (n1 != n2 && n1 < n2)
-			{
-				putchar(n1 + 48);
-				putchar(n2 + 48);
-
-				if (n1 + n2 != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			++n2;
+			putchar(',');
+			putchar(' ');
 		}
-		++n1;
 	}
 	putchar('\n');
+
 	return (0);
-}
