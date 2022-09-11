5dc2d39aa7f6682860c02bad035c84f27347bdfc
GodwinZuberu <onimico4eva@gmail.com>
2022-09-11 16:02:44 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,12 +6,12 @@
 * Inventing is a combination of brains and materials. The more brains you use, the less material you need
 *
 * Return: 0
-*
 */
 
 int main(void)
 {
 	int d;
+
 	for (d = 0; d < 100; d++)
 	{
 		putchar((d / 10) + '0');
