659c49679d00fe9448bc99350edba42e2f09f38c
GodwinZuberu <onimico4eva@gmail.com>
2022-09-12 21:26:58 -0700

    last one

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+*
+* main - entry point
+*
+* description - a simple program
+*
+*
+* Return 0
+*/
+int main(void)
+{
+	int n1 = 0, n2;
+	while (n1 < 98)
+	{
+		n2 = n1;
+		while (n2 <= 99)
+		{
+			if (n2 != n1)
+			{
+				putchar((n1 / 10) + 48);
+				putchar((n1 % 10) + 48);
+				putchar(' ');
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
+
+				if (n1 == 98 || n2 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++n2;
+		}
+		++n1;
+	}
+
+}
