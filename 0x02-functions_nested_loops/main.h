9022d68064538a5f42fae16684df81dff42e50e0
GodwinZuberu <onimico4eva@gmail.com>
2022-09-13 20:22:46 -0700

    adding the main file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUDDED
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+void _islower(int c);
+
+void isalpha(int c);
+
+void print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+#endif
+
