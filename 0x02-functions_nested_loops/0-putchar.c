8410ae71968bed3ccadeeeb8e78d718f486e7532
GodwinZuberu <onimico4eva@gmail.com>
2022-09-14 01:58:33 -0700

    review task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 /*
 * main : entrty point
 *
-* description - Write a program that prints _putchar, followed by a new line
+* description: Write a program that prints _putchar, followed by a new line
 * Return : 0
 */
 int main(void)
