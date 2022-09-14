033766d36ad2a80165afd36dcd49b73c15f08af9
GodwinZuberu <onimico4eva@gmail.com>
2022-09-13 21:46:36 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,13 +12,13 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		last_digit = (-1 * (n % 10));
-		_putchar (lastdigit + '0');
-		return (lastdigit);
+		_putchar (last_digit + '0');
+		return (last_digit);
 	}
 
 	else
 	{
-		lastdigit = (n % 10);
+		last_digit = (n % 10);
 		_putchar (last_digit + '0');
 		return (last_digit);
 	}
