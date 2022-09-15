e662200b6bebd273320e2d4162045a3f812b760a
GodwinZuberu <onimico4eva@gmail.com>
2022-09-14 20:29:45 -0700

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* description - Write a function that adds two integers and returns the result.
+* Return: Always 0.
+*/
+
+int add(int i, int k)
+{
+	return (i + k);
+}
