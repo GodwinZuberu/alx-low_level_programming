021f03f96b508be2889ebe99c98d266726d44e74
GodwinZuberu <onimico4eva@gmail.com>
2022-09-14 20:34:21 -0700

    done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
 /**
 * main - entry point
 *
-* add - prints addition of two numbers
+* @add - prints addition of two numbers
 * @i: holds the value of i
 * @k: holds the value of k
 * Return: Always 0.
