8b4100f7e31e37d381db5e8ae72e9f903d970d79
GodwinZuberu <onimico4eva@gmail.com>
2022-09-15 20:36:55 -0700

    done

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,15 +15,15 @@ if ((year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
 {
 if (month >= 3 && day >= 60)
 {
-days++;
+day++;
 }
 
 printf("Day of the year: %d\n", day);
-printf("the remaining days: %d\n", 366 - days);
+printf("the remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month == 2 &&day == 60)
+if (month == 2 && day == 60)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
