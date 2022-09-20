e5172062123e1ab18747b544726ecea703e9a0d8
GodwinZuberu <onimico4eva@gmail.com>
2022-09-19 19:37:00 -0700

    done

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,8 @@
 /**
 * main - check the code
 * @n: Parameter
-* description - Don't swap horses in crossing a stream
+* 
+* description - Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
 * Return: nothing
 */
 
