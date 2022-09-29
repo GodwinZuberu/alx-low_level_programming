945c4de8810fa6160aa401e3cd1a0b03c23f06d4
GodwinZuberu <onimico4eva@gmail.com>
2022-09-28 18:06:40 -0700

    done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,14 @@
 #include "main.h"
+
+int is_prime(int n, int start)
+{
+	if (start <= 1) 
+		return (1);
+	else if (n % start == 0)
+		return (0);
+	return (is_prime(n, start - 1));
+}
+
 /**
  *is_prime_number - returns the 1 if n is prime
  *@n: number to be checked
@@ -21,11 +31,3 @@ int is_prime_number(int n)
  *
  * Return: 1 if n is prime, 0 otherwise
  */
-int is_prime(int n, int start)
-{
-	if (start <= 1)
-		return (1);
-	else if (n % start == 0)
-		return (0);
-	return (is_prime(n, start - 1));
-}
