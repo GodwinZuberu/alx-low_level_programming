4b6a56d2a7e0f1487efcb788c877f5f62e65156b
GodwinZuberu <onimico4eva@gmail.com>
2022-09-28 17:45:57 -0700

    done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,14 +1,7 @@
 #include "main.h"
 
-int check_prime(int, int);
-/**
- *is_prime_number -> prime identifier
- *
- */
-int is_prime_number(int n)
-{
-	return (check_prime(n, l));
-}
+int is_divisible(int num, int div);
+int is_prime_number(int n);
 
 /**
  * check prime 0> it check prime number
@@ -16,13 +9,13 @@ int is_prime_number(int n)
  *@i: iterator
  *Return: a 1 or 0
  */
-int check_prime(int n, int i)
+int is_divisible(int num, int div)
 {
-	if (n <= l)
+	if (num % div == 0)
 		return (0);
-	if (n % i == 0 && i > l)
-		return (0);
-	if ((n / i) < i)
-		return (l);
-	return (check_prime(n, i + l));
+
+	if (div == num / 2)
+		return (1);
+
+	return (is_divisible(num, div + 1));
 }
