6aed74a521410b12d4d7cdbb19f3e5c53f787682
GodwinZuberu <onimico4eva@gmail.com>
2022-09-30 03:50:13 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#inlcude <stdio.h>
+
+/**
+ * main -> this is a fucntion to print its name 
+ * @argc: argc parameter
+ * @argv: an array of the command listed
+ * Return : 0 for success
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+This is a read me file for 0x0A-argc_argv folder 
+
+0x0A. C - argc, argv
+
+ By: Julien Barbier
