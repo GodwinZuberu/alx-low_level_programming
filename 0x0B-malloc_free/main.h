57674d01d7cf9e0fdce6e46e5d5cb11f5e3e1d95
GodwinZuberu <onimico4eva@gmail.com>
2022-10-04 10:45:34 -0700

    done

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,7 @@
 char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
-char *strdup(char *str);
+char *_strdup(char *str);
 char *argstostr(int ac, char **av);
 int **alloc_grid(int width, int height);
 
