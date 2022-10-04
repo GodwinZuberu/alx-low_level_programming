059407a9ce3180cfce7a35a00c728834a27f20b3
GodwinZuberu <onimico4eva@gmail.com>
2022-10-04 10:41:42 -0700

    done

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,11 @@
-#idndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-void free_grid(int **grid, int height);
 char *str_concat(char *s1, char *s2);
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+char *strdup(char *str);
+char *argstostr(int ac, char **av);
+int **alloc_grid(int width, int height);
 
+#endif
