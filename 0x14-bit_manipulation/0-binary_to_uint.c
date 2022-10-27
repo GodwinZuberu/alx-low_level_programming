237c1e39a4a458727d9a150a1415a0509d2e39c0
GodwinZuberu <zuberuonimisi@outlook.com>
2022-10-27 15:28:46 -0700

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,19 +6,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num;
+	unsigned int dec_val = 0;
+	int i;
 
 	if (!b)
 		return (0);
 
-	for (num = 0; *b; b++)
+	for (i = 0; b[i]; i++)
 	{
-		if (*b == '1')
-			num = (num << 1) | 1;
-		else if (*b == '0')
-			num <<= 1;
-		else
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (0);
+	return (dec_val);
 }
