4ad327706829f26396a5fd665d55e8a115113bcb
GodwinZuberu <zuberuonimisi@outlook.com>
2022-10-30 04:26:55 -0700

    hot fix

--- 0x13-more_singly_linked_lists/Lists.h
+++ 0x13-more_singly_linked_lists/Lists.h
@@ -1,16 +1,24 @@
 #ifndef _LISTS_
 #define _LISTS_
 
-#include <studio.h>
-#include <studio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
  *
- * Description: singly linked list noede structure
- */
+ *  * struct listint_s - singly linked list
+ *
+ *   * @n: integer
+ *
+ *    * @next: points to the next node
+ *
+ *     *
+ *
+ *      * Description: singly linked list node structure
+ *
+ *       * for Holberton project
+ *
+ *        */
 
 typedef struct listint_s
 {
@@ -19,17 +27,20 @@ typedef struct listint_s
 } listint_t;
 
 /**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
  *
- * Description: singly linked list of pointers
+ *  * struct listp_s - singly linked list
+ *
+ *   * @p: pointers of nodes
+ *
+ *    * @next: points to the next node
+ *      * Description: singly linked list of pointers
  */
+
 typedef struct listp_s
 {
-	void *p;
-	struct listp_s *next;
-}listp_t;
+		void *p;
+		struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -47,4 +58,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif /*LISTS_H*/
+
+#endif
